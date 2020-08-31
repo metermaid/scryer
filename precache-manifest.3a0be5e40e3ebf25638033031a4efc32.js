8e842f624aed7095c4df864652495a9cf1289ed8
Rita C <718216+metermaid@users.noreply.github.com>
2020-08-31 17:10:20 -0400

    Updates

--- precache-manifest.3a0be5e40e3ebf25638033031a4efc32.js
+++ precache-manifest.3a0be5e40e3ebf25638033031a4efc32.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "7a07c57ac1b27d8a315036fdbeb748ed",
+    "revision": "4424b37f4a6eb77cd8be306c6f9697c3",
     "url": "/scryer/index.html"
   },
   {
-    "revision": "463958154b81c77a731d",
+    "revision": "106a86384f723be37f6a",
     "url": "/scryer/static/css/2.04ff9426.chunk.css"
   },
   {
-    "revision": "535bb68739ead471e130",
+    "revision": "426d4b7698aa8fd0e0c1",
     "url": "/scryer/static/css/main.f17a2168.chunk.css"
   },
   {
-    "revision": "463958154b81c77a731d",
-    "url": "/scryer/static/js/2.844a6d82.chunk.js"
+    "revision": "106a86384f723be37f6a",
+    "url": "/scryer/static/js/2.729a14e8.chunk.js"
   },
   {
     "revision": "3453b8997016469371284a28c0e873e2",
-    "url": "/scryer/static/js/2.844a6d82.chunk.js.LICENSE.txt"
+    "url": "/scryer/static/js/2.729a14e8.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "535bb68739ead471e130",
-    "url": "/scryer/static/js/main.091f171b.chunk.js"
+    "revision": "426d4b7698aa8fd0e0c1",
+    "url": "/scryer/static/js/main.efd9f0b3.chunk.js"
   },
   {
     "revision": "5fbb6f7bf3f34e492ce4",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/scryer/precache-manifest.5ccaae81474ec0930c108691a3d26236.js"
+  "/scryer/precache-manifest.3a0be5e40e3ebf25638033031a4efc32.js"
 );
 
 self.addEventListener('message', (event) => {
