dcd1ec27e83c6e0d9bb359bcffc4f00dd238a9fa
Rita C <718216+metermaid@users.noreply.github.com>
2020-09-02 22:42:56 -0400

    Updates

---
+++ precache-manifest.68927143d8fbdab11cbc157dca54d748.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc2dc8f6edc1be50f34d058cf2dbf154",
+    "url": "/scryer/index.html"
+  },
+  {
+    "revision": "2fa9f3925600ce41d77f",
+    "url": "/scryer/static/css/2.04ff9426.chunk.css"
+  },
+  {
+    "revision": "f6d81e21ff2e277a1754",
+    "url": "/scryer/static/css/main.f17a2168.chunk.css"
+  },
+  {
+    "revision": "2fa9f3925600ce41d77f",
+    "url": "/scryer/static/js/2.d781ae20.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/scryer/static/js/2.d781ae20.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6d81e21ff2e277a1754",
+    "url": "/scryer/static/js/main.b7bae6cc.chunk.js"
+  },
+  {
+    "revision": "5fbb6f7bf3f34e492ce4",
+    "url": "/scryer/static/js/runtime-main.d451fea5.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/scryer/precache-manifest.bc54df39c3981646aac68e9b0bf8e744.js"
+  "/scryer/precache-manifest.68927143d8fbdab11cbc157dca54d748.js"
 );
 
 self.addEventListener('message', (event) => {
